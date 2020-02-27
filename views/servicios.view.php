498eb3d3b333d8f346f79c02c1fa50537a79c314
unknown <ame171337@gmail.com>
2020-02-27 14:52:50 -0600

    actualizando pag

--- views/servicios.view.php
+++ views/servicios.view.php
@@ -15,7 +15,153 @@
 <body>
   <?php require 'nav-bar.view.php'; ?>
 
-   
+  <section class="img-back-header h-md-back-2 py-4">
+    <div class="content-back-header container mx-auto">
+      <h2>Nuestros servicios</h2>
+      <hr>
+      <p class="text-back-header">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, et nam tenetur reiciendis minus nesciunt rem
+        amet impedit nulla qui, fugiat earum perferendis iusto vitae.(definir).
+      </p>
+    </div>
+  </section>
+
+  <main class="py-4">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="text-center">Modelo Educativo</h2>
+          <p class="pt-3 text-horario">En el horario de 08:00 a 14:00 hrs, ofrecemos: </p>
+        </div>
+      </div>
+      <div class="row pt-4 content-model">
+        <div class="opc-model col-12 col-md-4 pb-4">
+          <img src="<?php echo PATH; ?>img/img1-service.png" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv">
+          <p>Programa establecido por la SEP</p>
+        </div>
+        <div class="opc-model col-12 col-md-4 pb-4">
+          <img src="<?php echo PATH; ?>img/img2-service.jpg" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv rounded-circle">
+          <p>Inglés</p>
+        </div>
+        <div class="opc-model col-12 col-md-4 pb-4">
+          <img src="<?php echo PATH; ?>img/img3-service.jpg" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv rounded-circle">
+          <p>Francés</p>
+        </div>
+        <div class="opc-model col-12 col-md-4 pb-4">
+          <img src="<?php echo PATH; ?>img/img4-service.jpg" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv rounded-circle">
+          <p>Educación Física</p>
+        </div>
+        <div class="opc-model col-12 col-md-4 pb-4">
+          <img src="<?php echo PATH; ?>img/img5-service.jpg" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv rounded-circle">
+          <p>Educación Musical</p>
+        </div>
+        <div class="opc-model col-12 col-md-4">
+          <img src="<?php echo PATH; ?>img/img6-service.jpg" alt="Imagen ilustrativa del servicio" class="img-fluid img-serv rounded-circle">
+          <p class="pt-1">Computación</p>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <div class="sec-after py-4">
+    <div class="container text-center">
+      <h3 class="text-center">Clases Afterschool</h3>
+      <p class="pt-3 text-horario">
+        Con el fin de complementar con el desarrollo de sus hijos ofrecemos las siguientes clases especiales, en el horario de 14:00 a 15:00 hrs. 
+        También ofrecemos algunos talleres para el público en general en el mismo horario. 
+      </p>
+      <div class="content-after container pt-4">
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-ingles.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Tareas dirigidas</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-frances.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Francés para niños</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-danza.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Danza Folklorica</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-danza.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Ballet</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-taekwondo.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Lima Lama</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-taekwondo.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Taekwondo</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-zumba.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Zumba</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-zumba.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Robótica</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-ingles.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Cultura General</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-frances.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Francés para adultos</h4>
+          </div>
+        </div>
+        <div class="item-extra mb-3">
+          <img src="<?php echo PATH; ?>img/imgs-ingles.jpg" alt="Imagen ilustrativa de" class="img-fluid">
+          <div class="content-extra">
+            <h4>Hebreo para adultos</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="img-back py-4">
+        <div class="content-service container mx-auto">
+            <div class="row">
+                <div class="col-12 col-md-8 offset-md-2 col-lg-7 offset-lg-0 col-xl-7 order-2 ">
+                    <img class="img-fluid pt-3 pt-lg-0" src="<?php echo PATH; ?>img/imgs-comida.jpg" alt="Foto del servicio de comida del preescolar">
+                </div>
+                <div class="col-12 col-lg-5 order-lg-2 py-xl-4">
+                    <h3>Comedor</h3>
+                    <p>
+                      El colegio JRF ofrece dos modalidades para consumir alimentos, una es a través de la cooperativa 
+                      escolar donde se venden complementos a su lunch y la otra, a petición de los padres, se les 
+                      proporciona comida completa en dos horarios: 10:30 am (Receso) y 2 pm (after School).
+                    </p>                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
   <?php require 'footer.view.php'; ?>
 
   <!--Archivos javascript para bootstrap -->
