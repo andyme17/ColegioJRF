130b3cf9a3b435e9f1ea508ebb8ef9c6aa41762c
unknown <ame171337@gmail.com>
2020-05-22 17:01:04 -0500

    se añade pagina de error

---
+++ views/error_gestor.view.php
@@ -0,0 +1,30 @@
+            <?php require 'side_bar.view.php'; ?>
+                <main class="py-5">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 offset-xl-1 col-xl-10">
+                                <div class="col-12 offset-md-1 col-md-10 text-center shadow" id="list-doc">
+                                    <h3 class="text-center text-danger">Error de conexión</h3>
+                                    <h4 class="pt-2 text-center text-danger">Por favor inténtelo más tarde.</h4>
+                                    <img src="<?php echo PATH; ?>img/icono-error.jpg" alt="Imagen de error" class="img-fluid icono-error">                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScript files for Bootstrap 4 -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/headroom.min.js"></script>
+    <script src="../js/side-bar.js"></script>
+
+    <!-- Custom javascript files -->
+    <script src="../js/form-testimonio.js"></script>
+</body>
+
+</html>
